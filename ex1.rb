3e51bc604debf795b273ba42fa74200058f8bd91
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-05 16:01:22 -0400

    done with study drills in exercise 1

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,8 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+puts "\n"
+
+name = "zsh"
+#"This is a comment"; 
+puts "hello, #{name}"
