2e98d957ff5458eaee526b1c3f18e390f86e132c
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-05 16:52:36 -0400

    finish exercise 5

--- ex5.rb
+++ ex5.rb
@@ -1,14 +1,23 @@
+def lbs_to_kg(lbs)
+  return 0.453592 * lbs
+end
+
+def in_to_cm(inch)
+  return 2.54 * inch
+end
+
 name = 'Zed A. Shaw'
 age = 35 # not a lie in 2009
-height = 74 # inches
-weight = 180 # lbs
+height = in_to_cm(74) # inches
+weight = lbs_to_kg(180) # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height} cm tall."
+puts "He's #{weight} kg heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
